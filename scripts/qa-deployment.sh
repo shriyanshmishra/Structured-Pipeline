81d464f65f615d2f266958990d842253281c5abb
Shriyansh <amitsdmishra10@gmail.com>
2025-07-24 15:28:43 +0530

    9 Commit

---
+++ scripts/qa-deployment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+git fetch origin qa
+git diff --name-only origin/qa...HEAD > changed_files.txt
+grep '^force-app/main/default/' changed_files.txt > filtered_changed_files.txt || true
+
+mkdir -p manifest
+node scripts/generate-package-xml.js filtered_changed_files.txt manifest/package.xml
+
+sf project deploy start \
+  --manifest manifest/package.xml \
+  --target-org myqaorg \
+  --test-level RunLocalTests \
+  --wait 40
